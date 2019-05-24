a43ae30766eb0a7f71e08f3ed14afd0c99092ebd
Masoud Hemmatpour <mhemmatp@cisco.com>
2019-05-24 13:28:25 +0200

    add user

--- Dockerfile
+++ Dockerfile
@@ -90,7 +90,6 @@ RUN rm -rf /var/lib/apt/lists/* \
   ####################################################
   && rm ${HICNLIGHT_PLUGIN_LIB}
 
-RUN echo 'root:1' | chpasswd
 
 #################################
 # Install hicn module in sysrepo
--- init.sh
+++ init.sh
@@ -9,7 +9,11 @@ done
 /usr/bin/vpp -c /etc/hicn/super_startup.config &>log.txt &
 sleep 20
 sysrepod -d -l 0 &
+sleep 5
 sysrepo-plugind -d -l 0 &
+sleep 5
 netopeer2-server -d -v 0 &
+sleep 5
+RUN echo 'root:1' | chpasswd
 trap "kill -9 $$" SIGHUP SIGINT SIGTERM SIGCHLD
 wait
