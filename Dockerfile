5292251f60aeced0beac0f90d445b4bdd4dd7a20
Luca Muscariello <muscariello@ieee.org>
2019-12-13 12:07:13 +0100

    adding arm64 builds ubuntu18
    Signed-off-by: Luca Muscariello <muscariello@ieee.org>

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM ubuntu:18.04
 
-RUN apt-get update && apt-get install -y curl libprotobuf-c1 libev4 libavl1 libssh-4
-
+RUN apt-get update && apt-get install -y curl
 RUN curl -s https://packagecloud.io/install/repositories/fdio/release/script.deb.sh | bash
-RUN apt-get update && apt-get install -y supervisor hicn-plugin vpp-plugin-dpdk
+RUN apt-get update && apt-get install -y supervisor hicn-plugin vpp-plugin-dpdk hicn-sysrepo-plugin
 
 RUN echo "deb [trusted=yes] https://dl.bintray.com/icn-team/apt-hicn-extras bionic main" | tee -a /etc/apt/sources.list
-RUN apt-get update && apt-get install -y libyang sysrepo libnetconf2 netopeer2-server
+RUN apt-get update && apt-get install -y libnetconf2 netopeer2-server
 
 WORKDIR /
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,36 +3,5 @@ apt-get update && apt-get install -y curl
 curl -s https://packagecloud.io/install/repositories/fdio/release/script.deb.sh | bash
 apt-get update && apt-get install -y hicn-plugin hicn-plugin-dev vpp libvppinfra libhicn-dev\
         vpp-plugin-core vpp-dev libparc libparc-dev python3-ply python python-ply
-
-# Install main packages
-apt-get install -y git cmake build-essential libpcre3-dev swig \
-  libprotobuf-c-dev libev-dev libavl-dev protobuf-c-compiler libssl-dev \
-  libssh-dev libcurl4-openssl-dev libasio-dev libconfig-dev --no-install-recommends openssh-server \
-  ###############################################
-  # Build libyang
-  ################################################
-  git clone https://github.com/CESNET/libyang --branch devel --depth 1
-  mkdir -p libyang/build
-  cd libyang/build && cmake -DCMAKE_BUILD_TYPE=Release ..
-  make -j 4 install && cd /home/vagrant 
-  ########################################################################################
-  # Build sysrepo
-  ########################################################################################
-  git clone https://github.com/sysrepo/sysrepo.git --branch devel --depth 1
-  mkdir -p sysrepo/build
-  cd sysrepo/build && cmake -DCMAKE_BUILD_TYPE:String="Release" ..
-  make -j 4 install && ldconfig && cd /home/vagrant
- ############################################################
-  # Build libnetconf2
-  ############################################################
-  git clone https://github.com/CESNET/libnetconf2 --branch devel --depth 1
-  mkdir -p libnetconf2/build
-  cd libnetconf2/build && cmake -DCMAKE_BUILD_TYPE=Release ..
-  make -j4 install && cd /home/vagrant
-  ############################################################
-  # Build Netopeer
-  ############################################################
-  git clone https://github.com/CESNET/Netopeer2 --branch devel-server --depth 1
-  mkdir -p Netopeer2/server/build
-  cd Netopeer2/server/build && cmake -DCMAKE_BUILD_TYPE=Release ..
-  make -j 4 install && cd /home/vagrant
+echo "deb [trusted=yes] https://dl.bintray.com/icn-team/apt-hicn-extras bionic main" | tee -a /etc/apt/sources.list
+apt-get update && apt-get install -y libyang sysrepo libnetconf2 netopeer2-server
