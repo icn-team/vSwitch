9f547b628c10e7a75ca6b99e1bfbce8a289eb49f
acompagn <acompagn@cisco.com>
2020-03-02 16:43:28 +0100

    Disabled dns-sec and installed collectd plugin

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,6 +2,7 @@
 apt-get update && apt-get install -y curl
 curl -s https://packagecloud.io/install/repositories/fdio/release/script.deb.sh | sudo bash
 apt-get update && apt-get install -y hicn-plugin hicn-plugin-dev vpp libvppinfra libhicn-dev\
-        vpp-plugin-core vpp-dev libparc libparc-dev python3-ply python python-ply vpp-plugin-dpdk
+        vpp-plugin-core vpp-dev libparc libparc-dev python3-ply python python-ply vpp-plugin-dpdk\
+        hicn-utils-memif hicn-collectd-plugins
 echo "deb [trusted=yes] https://dl.bintray.com/icn-team/apt-hicn-extras bionic main" | tee -a /etc/apt/sources.list
 apt-get update && apt-get install -y libyang sysrepo libnetconf2 netopeer2-server
--- scripts/vagrant-box.sh
+++ scripts/vagrant-box.sh
@@ -4,8 +4,9 @@ export VAGRANT_DEFAULT_PROVIDER=$1
 vagrant up
 vagrant ssh -c "sudo bash /vagrant/bootstrap.sh"
 vagrant ssh -c "sudo apt-get clean && sudo apt-get purge &&\
-                sudo dd if=/dev/zero of=/EMPTY bs=1M &&\
+                sudo dd if=/dev/zero of=/EMPTY bs=1M;\
                 sudo rm -f /EMPTY &&\
+                sudo rm -f /etc/systemd/resolved.conf &&\
                 cat /dev/null > ~/.bash_history && history -c"
 vagrant package --output hicn-vnf.box
 
